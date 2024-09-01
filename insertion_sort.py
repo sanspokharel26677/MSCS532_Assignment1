0f3bc85e5d830278dd5f5cb5815e81e20a7e3083
sanspokharel <sanspokharel94@gmail.com>
2024-09-01 14:58:43 -0500

    code for monotonically decreasing insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,3 +4,28 @@ Author: Sandesh Pokharel
 Date: 2024-08-31
 Description: This is program for the Insertion Sort Algorithm that sorts in monotonically decreasing order
 """
+
+def insertion_sort_monotonically_decreasing(arr):
+    """
+    Sorts an array in monotonically decreasing order using the Insertion Sort algorithm.
+    
+    Parameters:
+    arr (list): The list of elements to be sorted.
+    
+    Returns:
+    list: The sorted list in monotonically decreasing order.
+    """
+    # Traversing from the second element to the end of the list
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        
+        # Moving elements of arr[0..i-1] that are less than key to one position ahead of their original position
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        
+        # This will be placing key in the correct position
+        arr[j + 1] = key
+    
+    return arr
